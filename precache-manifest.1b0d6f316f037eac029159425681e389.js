6e6fcca39cbd2d17cc0c9197c59107afc52b767b
Elias Argenal <elias.argenal101@gmail.com>
2024-10-20 00:35:28 -0500

    Updates

---
+++ precache-manifest.1b0d6f316f037eac029159425681e389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffbe9e1c3eb15f052a1d20fefc3293f",
+    "url": "/FronteraWebsite/index.html"
+  },
+  {
+    "revision": "70c8343ec2a6253f15b0",
+    "url": "/FronteraWebsite/static/css/main.4fe5045b.chunk.css"
+  },
+  {
+    "revision": "bcd6b5d72150101ea3e3",
+    "url": "/FronteraWebsite/static/js/2.c69218ed.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/FronteraWebsite/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "70c8343ec2a6253f15b0",
+    "url": "/FronteraWebsite/static/js/main.9e4fe97a.chunk.js"
+  },
+  {
+    "revision": "1d06920e1a544fe91059",
+    "url": "/FronteraWebsite/static/js/runtime~main.57766d82.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/FronteraWebsite/static/media/logo.06e73328.svg"
+  }
+]);
