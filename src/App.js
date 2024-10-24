8fb5d88a26de52cdde09ef0611a5db5baf46ef77
EliasArgenal <elias.argenal101@gmail.com>
2024-10-23 23:40:58 -0500

    reverted stay logged in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom'; // to switch between pages
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword,signInWithPopup } from 'firebase/auth';
 import { auth,googleProvider } from './firebase-config'; // Firebase config
-import {doc,setDoc} from "firebase/firestore";
+import {doc,setDoc, updateDoc } from "firebase/firestore";
 import {db} from "./firebase-config"; // import firestore instance
 import logo from './SmartWallet.png'; // You can use a banking-related logo instead of React's
 import { useEffect } from 'react';  // Add this to the top of your imports in App.js
